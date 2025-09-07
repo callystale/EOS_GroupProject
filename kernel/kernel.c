601a72c40627925b5ce962756a1370fa0aab04e0
Dohwan466 <s3878104@rmit.edu.vn>
2025-09-07 14:02:23 +0900

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,32 +2,87 @@
 #include "../uart/uart1.h"
 #include "mbox.h"
 #include "framebf.h"
+#include "background.h"
+#include "menu.h"
 
-void main()
+#define FONT_HEIGHT 8
+
+extern unsigned int width, height;   // Global variable
+
+
+void display_backgroundandtitle(void) {
+    drawImage(background_bitmap_Chicken_Gunny, 0, 0, BG_WIDTH, BG_HEIGHT);
+    render_title_top("CHICKEN GUNNY");
+}
+
+/* Function for displaying menu box on the screen */
+void show_main_menu(void) {
+    // The list of menu
+    const char* MAIN_ITEMS[] = { "Start Game", "Options", "Credits", "Exit" };
+
+    // Menu box location/size
+    int boxX = 20;
+    int boxW = (int)width - 40;
+    int boxH = 4 * (FONT_HEIGHT + 10) + 24;   // The height of menu items
+    int boxY = (int)height - boxH - 20;
+
+    // Menu initialization
+    Menu m;
+    menu_init_region(&m, MAIN_ITEMS, 4, boxX, boxY, boxW, boxH);
+
+    // Blocking input loop(whenever inputting, redraw)
+    int in_menu = 1;
+    while (in_menu) {
+        // 1) Background + Title
+        display_backgroundandtitle();
+        // 2) Menu Box render
+        menu_render(&m);
+
+        // 3) input(if not, blocking by uart_getc)
+        char c = uart_getc();
+
+        // 4) Processing
+        if (c=='w' || c=='k' || c=='s' || c=='j') {
+            menu_handle_key(&m, c);   // Move
+        } else if (c=='\r' || c=='\n') {
+            switch (m.selected) {
+                case 0: // Start Game
+                    uart_puts("\r\n[MENU] Start Game selected\r\n");
+                    in_menu = 0;      // Menu exit → go to the the next step(Game)
+                    break;
+                case 1: // Options
+                    uart_puts("\r\n[MENU] Options selected\r\n");
+                    // TODO: Options screeen (it can be Character Selection)
+                    break;
+                case 2: // Credits
+                    uart_puts("\r\n[MENU] Credits selected\r\n");
+                    // TODO: Credits screen (if not needed, please get rid of it)
+                    break;
+                case 3: // Exit
+                    uart_puts("\r\n[MENU] Exit selected\r\n");
+                    qemu_exit_semihosting(0); // Exit
+                    in_menu = 0;
+                    break;
+            }
+        }
+        // Ignore the others
+    }
+    
+}
+
+void main(void)
 {
-    // set up serial console
-	uart_init();
+    
+    uart_init();
+    uart_puts("\n\nHello World!\n");
 
-	// say hello
-	uart_puts("\n\nHello World!\n");
+    
+    framebf_init();
+    // show the main menu
+    show_main_menu();
 
-	// Initialize frame buffer
-	framebf_init();
-
-	// Draw something on the screen
-	drawRectARGB32(100,100,400,400,0x00AA0000,1); //RED
-	drawRectARGB32(150,150,400,400,0x0000BB00,1); //GREEN
-	drawRectARGB32(200,200,400,400,0x000000CC,1); //BLUE
-	drawRectARGB32(250,250,400,400,0x00FFFF00,1); //YELLOW
-	drawPixelARGB32(300, 300, 0x00FF0000); //RED
-
-
-	// echo everything back
-	while(1) {
-		//read each char
-		char c = uart_getc();
-
-		//send back 
-		uart_sendc(c);
-	}
+    while (1) {
+        char c = uart_getc();
+        uart_sendc(c);
+    }
 }

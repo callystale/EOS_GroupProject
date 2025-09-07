d776dab55974a0c2b54417035c2189301b7e0688
Dohwan466 <s3878104@rmit.edu.vn>
2025-09-07 14:00:43 +0900

    Create font.h

---
+++ kernel/font.h
@@ -0,0 +1,9 @@
+enum {
+    FONT_WIDTH     = 8,  // Width in pixels
+    FONT_HEIGHT    = 8,  // Height in pixel
+    FONT_BPG       = 8,  // Bytes per character glyph
+    FONT_BPL       = 1,  // Bytes per line
+    FONT_NUMGLYPHS = 224 // Total number of characters in the font
+};
+
+extern unsigned char font[FONT_NUMGLYPHS][FONT_BPG];
